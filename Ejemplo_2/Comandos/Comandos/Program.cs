207bde0c880c8924e572479c4713d840ffda575e
alejo37077 <93800328+alejo37077@users.noreply.github.com>
2021-11-12 17:13:09 -0500

    Octubre
    Primer coming de este repositorio

---
+++ Ejemplo_2/Comandos/Comandos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Comandos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un numero");
+             int num=Console.Read();
+
+             Console.WriteLine("El Ascill es = "+num);
+
+
+        }
+    }
+}
