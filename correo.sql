54cbb9fcfd00bf49de549221780d807c4c099571
alejo37077 <93800328+alejo37077@users.noreply.github.com>
2022-02-11 23:15:29 -0500

    base de datos
    BD

---
+++ correo.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-02-2022 a las 00:11:29
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `correo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bandeja`
+--
+
+CREATE TABLE `bandeja` (
+  `id` int(10) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `description` varchar(350) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `bandeja`
+--
+
+INSERT INTO `bandeja` (`id`, `name`, `description`) VALUES
+(1, 'jh@jh.com', 'Hola');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `usuario` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `rol` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`usuario`, `pass`, `rol`) VALUES
+('Alejandro', '1234', 'Admin'),
+('Jhon', '1234', 'Usuario');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bandeja`
+--
+ALTER TABLE `bandeja`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
